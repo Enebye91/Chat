6f5414d70c34135ad374f737b2768f88eaa30e54
Enebye91 <eaamivj@students.eaaa.dk>
2024-08-07 11:07:49 +0200

    Connecting socket.io server with frontend try-on

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,31 @@
 // import { useState } from 'react'
 
 import "./App.css";
+import io from "socket.io-client";
+import { useEffect } from "react";
+
+// variabel til backend server
+// Man kan bruge den til at listen to an event
+const socket = io.connect("http://localhost:5174");
 
 function App() {
+  // Der er ikke noget som hedder emit i react, og det er her socket.io kommer ind
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "Hello" }); // Her kan vi sende noget data, hvad at sætte et object til eventet.
+  };
+
+  // Her kan man listen til eventet fra backenden ved et useEffect Hook, hver gang at der bliver smidt et event over til os
+  // Så hver gang at et event er emittet, køre denne function med socket variablen
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    });
+  }, [socket]);
+
   return (
     <>
       <input placeholder="Message..." />
-      <button> Send </button>
+      <button onClick={sendMessage}> Send </button>
     </>
   );
 }
--- Server/index.js
+++ Server/index.js
@@ -17,12 +17,24 @@ const server = http.createServer(app);
 // new server, fordi server er en class ovenover
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
 
-// React køre på port 3000, derfor 3001
-server.listen(3001, () => {
+// Listen to an event. Så lige så snart en user logger ind, vil det her starte med at køre.
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("send_message", (data => {
+   // her skal alt den emit til alle som er connected til serveren. broadcast gør at man kan sende til alle andre end en selv. 
+   socket.broadcast.emit("receive_message, data") // Man giver den en ny event, som bliver listen til i frontend
+
+
+  })); // callback function som vil modtage dataen fra objectet i App.jsx
+});
+
+// React køre på port 5173, derfor 5174
+server.listen(5174, () => {
   console.log("Server is running");
 });
